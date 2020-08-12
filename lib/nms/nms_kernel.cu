fd282efe6610b6a301db72b6a591c9c9b627760b
honestabelink <honestabelink@gmail.com>
2020-08-12 15:07:25 -0400

    Windows Build Support
    Adds windows build support via pytorch cuda build extensions.
    
    Performance was not evaluated, but the results appear reasonable.

--- lib/nms/nms_kernel.cu
+++ lib/nms/nms_kernel.cu
@@ -8,6 +8,13 @@
 #include <vector>
 #include <iostream>
 
+#if defined(__linux__)
+  #define NMS_TYPE int
+#endif
+#if defined(_WIN64)
+  #define NMS_TYPE long
+#endif
+
 #define CUDA_CHECK(condition) \
   /* Code block avoids redefinition of cudaError_t error */ \
   do { \
@@ -87,7 +94,7 @@ void _set_device(int device_id) {
   CUDA_CHECK(cudaSetDevice(device_id));
 }
 
-void _nms(int* keep_out, int* num_out, const float* boxes_host, int boxes_num,
+void _nms(NMS_TYPE* keep_out, int* num_out, const float* boxes_host, int boxes_num,
           int boxes_dim, float nms_overlap_thresh, int device_id) {
   _set_device(device_id);
 
